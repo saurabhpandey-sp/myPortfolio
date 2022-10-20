1a1ee70f8aa463cdca245db17ed59a3fd8c6b9c1
saurabhpandey-sp <103130980+saurabhpandey-sp@users.noreply.github.com>
2022-10-20 20:35:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <>
     <Router>
         <Routes>
-         <Route path="/myPortfolio" element={<><Hero/><Abouts/><Cards/><Skills/><Contacts/><Footers/></>} />
+         <Route path="/" element={<><Hero/><Abouts/><Cards/><Skills/><Contacts/><Footers/></>} />
        </Routes>
      </Router>
     </>
